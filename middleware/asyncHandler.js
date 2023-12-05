cd633357f993fdbf3ccb3ca0a00c82ecb8455bd7
Galerdene <galerdenechinbat@gmail.com>
2023-12-05 21:34:31 +0800

    add logger

--- middleware/asyncHandler.js
+++ middleware/asyncHandler.js
@@ -1,4 +1,6 @@
-const asyncHandler = (fn) => (req, res, next) =>
-  Promise.resolve(fn(req, res, next)).catch(next);
+const asyncHandler = (fn) => (req, res, next) => {
+  console.log(`Request: ${req.method} ${req.originalUrl}${req.url}`);
+  return Promise.resolve(fn(req, res, next)).catch(next);
+};
 
 module.exports = asyncHandler;
